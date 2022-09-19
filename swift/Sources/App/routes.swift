7ae0899703d6fa29034e70e63a37f51ca9b8c869
Durran Jordan <durran@gmail.com>
2022-09-19 14:42:37 -0400

    fix: fixing routes from conflicts

--- swift/Sources/App/routes.swift
+++ swift/Sources/App/routes.swift
@@ -1,15 +1,35 @@
 import Vapor
+import MongoDBVapor
 
 let nealController = NealController()
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
+  app.get { req async in
+    "It works!"
+  }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+  app.get("feed-messages") { req async throws -> [FeedMessage] in
+    try await req.findFeedMessages()
+  }
 
-    try? nealController.boot(routes: app.routes)
+  app.webSocket("feed") { req, ws in
+    print(ws)
+  }
+
+  try? nealController.boot(routes: app.routes)
+}
+
+extension Request {
+
+  var feedMessageCollection: MongoCollection<FeedMessage> {
+    self.application.mongoDB.client.db("mta").collection("feedMessages", withType: FeedMessage.self)
+  }
+
+  func findFeedMessages() async throws -> [FeedMessage] {
+    do {
+      return try await self.feedMessageCollection.find().toArray()
+    } catch {
+      throw Abort(.internalServerError, reason: "Failed to load feed messages: \(error)")
+    }
+  }
 }
