6db189a7c8b7f3ef3d47099e461bccf0c37d92f0
Nic Raboy <nicolas.raboy@mongodb.com>
2022-09-19 12:32:31 -0700

    Tensorflow Uploads and MongoDB Tensorflow Metadata
    The `/upload` endpoint will take an image, upload it to the Tensorflow container, insert the results into MongoDB, and then return the results back to the client that made the upload.

--- go/gosrc/main.go
+++ go/gosrc/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"time"
@@ -14,11 +19,88 @@ import (
 )
 
 var client *mongo.Client
+var collection *mongo.Collection
 
+type TensorflowClassifyResult struct {
+	Filename string                  `json:"filename" bson:"filenamme"`
+	Labels   []TensorflowLabelResult `json:"labels" bson:"labels"`
+}
+
+type TensorflowLabelResult struct {
+	Label       string  `json:"label" bson:"label"`
+	Probability float32 `json:"probability" bson:"probability"`
+}
+
+/*
+ * SEND IMAGE TO TENSORFLOW SERVICE AND ADD RESULT TO MONGODB
+ */
+func UploadImageEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	file, header, err := request.FormFile("image")
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer file.Close()
+	data, err := uploadFile("http://localhost:8080/recognize", header.Filename, file)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	// STRETCH GOAL: TENSORFLOW COMPLETE, UPLOAD IMAGE TO S3
+	var tensorflowResult TensorflowClassifyResult
+	_ = json.NewDecoder(bytes.NewReader(data)).Decode(&tensorflowResult)
+	_, err = collection.InsertOne(context.Background(), tensorflowResult)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	response.Write(data)
+	return
+}
+
+func uploadFile(url string, filename string, file multipart.File) ([]byte, error) {
+	httpRequestBody := &bytes.Buffer{}
+	formWriter := multipart.NewWriter(httpRequestBody)
+	writer, err := formWriter.CreateFormFile("image", filename)
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(writer, file)
+	if err != nil {
+		return nil, err
+	}
+	formWriter.Close()
+	httpClient := &http.Client{}
+	httpRequest, err := http.NewRequest("POST", url, bytes.NewReader(httpRequestBody.Bytes()))
+	if err != nil {
+		return nil, err
+	}
+	httpRequest.Header.Set("content-type", formWriter.FormDataContentType())
+	httpResponse, err := httpClient.Do(httpRequest)
+	if err != nil {
+		return nil, err
+	}
+	data, _ := ioutil.ReadAll(httpResponse.Body)
+	return data, nil
+}
+
+/*
+ * START AND CONFIGURE MONGODB CONNECTION AND WEB SERVICE ROUTING
+ */
 func main() {
 	fmt.Println("Starting the application...")
 	if os.Getenv("MONGODB_URI") == "" {
 		log.Fatal("Missing the `MONGODB_URI` environment variable!")
+	}
+	if os.Getenv("MONGODB_DATABASE") == "" {
+		log.Fatal("Missing the `MONGODB_DATABASE` environment variable!")
+	}
+	if os.Getenv("MONGODB_COLLECTION") == "" {
+		log.Fatal("Missing the `MONGODB_COLLECTION` environment variable!")
 	}
 	fmt.Println("Connecting to the database...")
 	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
@@ -30,9 +112,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer client.Disconnect(ctx)
+	collection = client.Database(os.Getenv("MONGODB_DATABASE")).Collection(os.Getenv("MONGODB_COLLECTION"))
+	defer func() {
+		fmt.Println("Disconnecting from the database...")
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 	fmt.Println("Linking the routers...")
 	router := mux.NewRouter()
+	router.HandleFunc("/upload", UploadImageEndpoint).Methods("POST")
 	fmt.Println("Serving at http://localhost:12345")
 	http.ListenAndServe(":12345", router)
 }
