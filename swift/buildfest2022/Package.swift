a265c1040cff8faa7a6a8a25b3fc8705276b2e7f
Durran Jordan <durran@gmail.com>
2022-09-19 12:11:26 -0400

    chore: add mongo swift driver dependency

--- swift/buildfest2022/Package.swift
+++ swift/buildfest2022/Package.swift
@@ -8,13 +8,15 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/mongodb/mongo-swift-driver.git", from: "1.3.1"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "MongoSwift", package: "mongo-swift-driver"),
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
@@ -23,7 +25,10 @@ let package = Package(
                 .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
-        .executableTarget(name: "Run", dependencies: [.target(name: "App")]),
+        .executableTarget(name: "Run", dependencies: [
+            .target(name: "App"),
+            .product(name: "MongoSwift", package: "mongo-swift-driver"),
+        ]),
         .testTarget(name: "AppTests", dependencies: [
             .target(name: "App"),
             .product(name: "XCTVapor", package: "vapor"),
