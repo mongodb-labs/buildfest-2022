40fa0733734fa6e21cd54fe0a351e1398b4a8bfa
Abraham Egnor <abraham.egnor@mongodb.com>
2022-09-19 11:40:48 -0400

    Connect to Atlas

---
+++ csharp/ghostsnek/HelloAtlas/Program.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connStr = String.Format("mongodb+srv://{0}:{1}@cluster0.gb1qy3e.mongodb.net/?retryWrites=true&w=majority", args[0], args[1]);
+            MongoClient dbClient = new MongoClient(connStr);
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+    }
+}
