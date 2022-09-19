7776202925409703719a32becac1c8f07abcf53e
Durran Jordan <durran@gmail.com>
2022-09-19 14:08:39 -0400

    feat: adding id to feed message

--- swift/buildfest2022/Sources/App/Models/FeedMessage.swift
+++ swift/buildfest2022/Sources/App/Models/FeedMessage.swift
@@ -1,7 +1,9 @@
 import Foundation
 import Vapor
+import MongoSwift
 
 struct FeedMessage: Content {
+  let _id: BSONObjectID
   let feedHeader: FeedHeader
   let feedEntity: FeedEntity
 }
--- swift/buildfest2022/Sources/App/routes.swift
+++ swift/buildfest2022/Sources/App/routes.swift
@@ -3,13 +3,17 @@ import Vapor
 let nealController = NealController()
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
+  app.get { req async in
+    "It works!"
+  }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+  app.get("hello") { req async -> String in
+    "Hello, world!"
+  }
+  
+  app.webSocket("feed") { req, ws in
+    print(ws)
+  }
 
-    try? nealController.boot(routes: app.routes)
+  try? nealController.boot(routes: app.routes)
 }
