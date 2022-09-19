8a1bcb511e25ab4bb034ac65fbe3e56bf40d1f4e
Abraham Egnor <abraham.egnor@mongodb.com>
2022-09-19 15:10:11 -0400

    inserting records

--- csharp/ghostsnek/HelloAtlas/Program.cs
+++ csharp/ghostsnek/HelloAtlas/Program.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
 
 namespace test
 {
@@ -6,16 +8,28 @@ namespace test
     {
         static void Main(string[] args)
         {
-            var connStr = String.Format("mongodb+srv://{0}:{1}@cluster0.gb1qy3e.mongodb.net/?retryWrites=true&w=majority", args[0], args[1]);
-            MongoClient dbClient = new MongoClient(connStr);
+            BsonClassMap.RegisterClassMap<Record>();
 
-            var dbList = dbClient.ListDatabases().ToList();
+            var connStr = String.Format("mongodb+srv://{0}:{1}@cluster0.gb1qy3e.mongodb.net/?retryWrites=true&w=majority", args[0], args[1]);
+            MongoClient client = new MongoClient(connStr);
+
+            var dbList = client.ListDatabases().ToList();
 
             Console.WriteLine("The list of databases on this server is: ");
             foreach (var db in dbList)
             {
                 Console.WriteLine(db);
             }
+
+            var coll = client.GetDatabase("buildfest2022").GetCollection<Record>("hello");
+            var rec = new Record { name = "Bob" };
+            coll.InsertOne(rec);
         }
+    }
+
+    class Record
+    {
+        public ObjectId id { get; set; }
+        public string? name { get; set; }
     }
 }
