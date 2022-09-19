9c5e8b8ea7a6c79e2655f6bd88b812bac4bc64c4
Neal Beeken <neal.beeken@mongodb.com>
2022-09-19 18:20:56 -0400

    feat: minimal data populate script

--- swift/Sources/MTA/main.swift
+++ swift/Sources/MTA/main.swift
@@ -1,14 +1,15 @@
 #! /usr/bin/env swift
 
-import MongoSwift
-
+import Foundation
 import MongoSwift
 import NIO
 
 let elg = MultiThreadedEventLoopGroup(numberOfThreads: 4)
 
-// replace the following string with your connection uri
+// // replace the following string with your connection uri
 let uri = "mongodb://localhost:27017/test"
+
+// replace the following string with your connection uri
 
 let client = try MongoClient(
     uri,
@@ -27,4 +28,28 @@ defer {
 // print a list of database names
 print(try client.listDatabaseNames().wait())
 
-// your application logic
+let db = client.db("mta")
+let subways = db.collection("subways")
+
+try subways.drop().wait()
+
+var feedWithIds: [BSONDocument] = Array()
+// iterate from i = 1 to 1 = 3
+for _ in 0...10 {
+    feedWithIds.append([
+        "feedHeader": [:],
+        "feedEntity": [
+            "vehiclePosition": [
+                "position": [
+                    "latitude":  BSON.double(Double.random(in: 0.0 ..< 100.0)),
+                    "longitude": BSON.double(Double.random(in: 0.0 ..< 100.0)),
+                    "bearing":   BSON.double(Double.random(in: 0.0 ..< 100.0)),
+                    "odometer":  BSON.double(Double.random(in: 0.0 ..< 100.0)),
+                    "speed":     BSON.double(Double.random(in: 0.0 ..< 100.0))
+                ]
+            ]
+        ]
+    ] as BSONDocument)
+}
+
+print(try subways.insertMany(feedWithIds).wait())
