2b0f177cc0d8b51c9fa766cb818f814e56be2dbc
Nic Raboy <nicolas.raboy@mongodb.com>
2022-09-19 09:52:42 -0700

    Fix Up Docker and Add Web Service Boilerplate
    Docker now has a Compose file. The boilerplate Go file is now configured for MongoDB and Gorilla Mux.

--- go/gosrc/main.go
+++ go/gosrc/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
 
 func main() {
-	fmt.Println("TESTING...")
+	fmt.Println("Starting the application...")
+	if os.Getenv("MONGODB_URI") == "" {
+		log.Fatal("Missing the `MONGODB_URI` environment variable!")
+	}
+	fmt.Println("Connecting to the database...")
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	fmt.Println("Linking the routers...")
+	router := mux.NewRouter()
+	fmt.Println("Serving at http://localhost:12345")
+	http.ListenAndServe(":12345", router)
 }
--- go/readme.md
+++ go/readme.md
@@ -1,3 +1,9 @@
+## INSTRUCTIONS [DOCKER]
+
+1. Duplicate the **docker-compose.yml.example** file and rename the new file **docker-compose.yml** so it doesn't get included in Git.
+2. Add the MongoDB Atlas URI string into the `MONGODB_URI` field of the **docker-compose.yml** file.
+3. Build and deploy the Go application with `docker-compose up` using the optional `-d` flag if you'd like to deploy in detached mode.
+
 ## TEAM
 
 - Anaiya Raisinghani
