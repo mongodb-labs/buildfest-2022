21e2686013c37cccb02e9bbfeadf50401cb2aebf
Neal Beeken <neal.beeken@mongodb.com>
2022-09-20 13:17:16 -0400

    feat: interval example

--- swift/Public/js/index.mjs
+++ swift/Public/js/index.mjs
@@ -2,17 +2,38 @@ import { EJSON } from 'https://unpkg.com/bson@4.7.0/dist/bson.browser.esm.js'
 
 const dateFmt = Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' });
 const stream = document.getElementById('mta-stream')
+
+let occurrences = 0;
+const interval = setInterval(() => {
+    stream.setState({
+        items: [
+            ...stream.state.items,
+            {
+                 created: dateFmt.format(new Date()),
+                ...Object.fromEntries(Array.from({ length: 4 }, (_, i) => Math.random()).entries())
+            }
+        ]
+    })
+
+    if (occurrences === 5) {
+        clearInterval(interval)
+    }
+    occurrences += 1;
+}, 3000)
+
 const websocket = new WebSocket(`ws://${window.location.host}/feed`)
 
 websocket.addEventListener('open', () => console.log('ws => open'))
 websocket.addEventListener('close', () => console.log('ws => close'))
 
 websocket.addEventListener('message', message => {
-    const data = EJSON.parse(message.data, {relaxed: false})
+    const data = EJSON.parse(message.data, { relaxed: false })
     stream.setState({
         items: [...stream.state.items, { created: dateFmt.format(new Date()), ...data }]
     })
 })
+
+websocket.send({pipeline,})
 
 websocket.addEventListener('error', error => {
     stream.setState({
