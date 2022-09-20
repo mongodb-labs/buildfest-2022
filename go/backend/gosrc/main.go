e548508eba6730165b590f29a9c74ffbca859a7e
Nic Raboy <nicolas.raboy@mongodb.com>
2022-09-20 11:52:19 -0700

    Hash File Uploads. Include Find Endpoint
    File uploads are now renamed to an ObjectID hash string. The original filename and hashed file name are stored in documents within MongoDB. Created an endpoint for finding all documents within the collection. Also doing cleanup by dropping the collection every time the application starts.

--- go/backend/gosrc/main.go
+++ go/backend/gosrc/main.go
@@ -11,9 +11,12 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -21,9 +24,10 @@ import (
 var client *mongo.Client
 var collection *mongo.Collection
 
-type TensorflowClassifyResult struct {
-	Filename string                  `json:"filename" bson:"filename"`
-	Labels   []TensorflowLabelResult `json:"labels" bson:"labels"`
+type ImageInformation struct {
+	OriginalFilename string                  `json:"filename" bson:"filename"`
+	DiskFilename     string                  `json:"disk_filename" bson:"disk_filename"`
+	TensorflowLabels []TensorflowLabelResult `json:"labels" bson:"labels"`
 }
 
 type TensorflowLabelResult struct {
@@ -32,7 +36,7 @@ type TensorflowLabelResult struct {
 }
 
 /*
- * SEND IMAGE TO TENSORFLOW SERVICE AND ADD RESULT TO MONGODB
+ * SEND IMAGE TO TENSORFLOW SERVICE AND ADD RESULT TO MONGODB. SAVE IMAGE TO FILESYSTEM.
  */
 func UploadImageEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
@@ -44,7 +48,7 @@ func UploadImageEndpoint(response http.ResponseWriter, request *http.Request) {
 	}
 	defer file.Close()
 	// STRETCH GOAL: TENSORFLOW COMPLETE, UPLOAD IMAGE TO S3
-	_, err = saveFile(header.Filename, file)
+	diskFileName, err := saveFile(header.Filename, file)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
@@ -56,8 +60,9 @@ func UploadImageEndpoint(response http.ResponseWriter, request *http.Request) {
 		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
 		return
 	}
-	var tensorflowResult TensorflowClassifyResult
+	var tensorflowResult ImageInformation
 	_ = json.NewDecoder(bytes.NewReader(data)).Decode(&tensorflowResult)
+	tensorflowResult.DiskFilename = diskFileName
 	_, err = collection.InsertOne(context.Background(), tensorflowResult)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
@@ -68,18 +73,39 @@ func UploadImageEndpoint(response http.ResponseWriter, request *http.Request) {
 	return
 }
 
-func saveFile(filename string, file multipart.File) (bool, error) {
-	diskFile, err := os.OpenFile("./images/"+filename, os.O_WRONLY|os.O_CREATE, os.ModePerm)
+/*
+ * RETURN ALL DOCUMENTS FROM MONGODB
+ */
+func FindImagesEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var images []ImageInformation
+	cursor, err := collection.Find(context.Background(), bson.M{})
 	if err != nil {
-		return false, err
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	if err = cursor.All(context.Background(), &images); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(images)
+}
+
+func saveFile(filename string, file multipart.File) (string, error) {
+	diskFile, err := os.OpenFile("./images/"+primitive.NewObjectID().Hex()+filepath.Ext(filename), os.O_WRONLY|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return "", err
 	}
 	defer diskFile.Close()
 	_, err = io.Copy(diskFile, file)
 	if err != nil {
-		return false, err
+		return "", err
 	}
 	file.Seek(0, io.SeekStart)
-	return true, nil
+	// return fmt.Sprintf("%x", md5.Sum([]byte(filename))), nil
+	return primitive.NewObjectID().Hex() + filepath.Ext(filename), nil
 }
 
 func uploadFile(url string, filename string, file multipart.File) ([]byte, error) {
@@ -143,9 +169,14 @@ func main() {
 			panic(err)
 		}
 	}()
+	fmt.Println("Purging old database collections...")
+	if err = collection.Drop(ctx); err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Linking the routers...")
 	router := mux.NewRouter()
 	router.HandleFunc("/upload", UploadImageEndpoint).Methods("POST")
+	router.HandleFunc("/find", FindImagesEndpoint).Methods("GET")
 	router.PathPrefix("/images/").Handler(http.StripPrefix("/images/", http.FileServer(http.Dir("./images/"))))
 	fmt.Println("Serving at http://localhost:12345")
 	http.ListenAndServe(":12345", router)
