dab7ca39b09585617d8df7be2080fd6f85f20961
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2022-09-21 22:15:44 +0530

    Added sample images & integrated mock APIs

--- go/client/src/pages/UploadPage.js
+++ go/client/src/pages/UploadPage.js
@@ -1,4 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+
+const BACKEND_HOST = process.env.BACKEND_HOST || "localhost";
+
 const UploadPage = () => {
+  const [file, setFile] = useState();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      const response = await axios.post(BACKEND_HOST, formData, {
+        headers: {},
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
   return (
     <div
       className="App"
@@ -6,8 +31,8 @@ const UploadPage = () => {
     >
       <h1>Upload Images</h1>
       <h2>This page allows you to upload an image to the app.</h2>
-      <input type="file" />
-      <button>Submit</button>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 };
