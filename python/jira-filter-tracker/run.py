eb6000530ae9fb07cd7e5f04332340d6b8b5031a
Neil Shweky <neilshweky@gmail.com>
2022-09-23 14:08:22 -0400

    update to prod

--- python/jira-filter-tracker/run.py
+++ python/jira-filter-tracker/run.py
@@ -9,6 +9,6 @@ config.read(os.path.abspath(os.path.join(".ini")))
 if __name__ == "__main__":
     app = create_app()
     app.config['DEBUG'] = True
-    app.config['MONGO_URI'] = config['TEST']['DB_URI']
+    app.config['MONGO_URI'] = config['PROD']['DB_URI']
 
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
